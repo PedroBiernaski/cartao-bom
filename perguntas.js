14df82102448ae30620c8f6dd30cd11032a3959f
PedroBiernaski <137187178+PedroBiernaski@users.noreply.github.com>
2024-10-24 09:55:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo Da Vinci'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a queda do Muro de Berlim?',
+    '1989'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta mais quente do nosso sistema solar?',
+    'Vênus'
+)
